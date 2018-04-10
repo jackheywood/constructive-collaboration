fea5caaf79554c5e07b4fbe9a8db96cdd8c63f78
jackheywood <jack.heywood@softwire.com>
2018-04-10 09:39:42 +0100

    Hide nav on scroll

---
+++ scripts/nav-hide.js
@@ -0,0 +1,33 @@
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('.navbar').outerHeight();
+
+var didScroll;
+
+$(window).scroll(event => {
+    didScroll = true;
+});
+
+setInterval(() => {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    if (Math.abs(lastScrollTop - st) <= delta) {
+        return;
+    }
+
+    if (st > lastScrollTop && st > navbarHeight) {
+        $('.navbar').removeClass('nav-down').addClass('nav-up');
+    } else {
+        if (st + $(window).height() < $(document).height()) {
+            $('.navbar').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+
+    lastScrollTop = st;
+}
